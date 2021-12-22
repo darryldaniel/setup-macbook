fb8ae5fc85237fd6a8879b607fb7bc19dc2ba8b0
Darryl Daniel <darrylndaniel@gmail.com>
2021-12-22 13:53:13 +0200

    :wrench: remove asdf

--- install-zulu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ -d "$HOME/.zulu" ]
-then
-    echo "OK::Zulu already installed, exiting"
-    exit 0
-fi
-http GET https://raw.githubusercontent.com/zulu-zsh/install/master/install | zsh && zsh
-echo "CHANGED::Zulu installed successfully"
